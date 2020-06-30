871c863f2b38be33e4734186464f3d3efd6c457e
PhillipsOlagunju <phillipsolagunju21@gmail.com>
2020-06-30 20:58:40 +0000

    introduce classes to contain a product set and a product

---
+++ project/src/main/java/com/google/sps/data/ProductItem.java
@@ -0,0 +1,15 @@
+package com.google.sps.data;
+
+
+
+public class ProductItem{
+    String productId;
+    String productName;
+    String productCategory;
+
+    public ProductItem(String productId, String ProductName, String ProductCategory){
+        this.productId = productId;
+        this.productName = productName;
+        this.productCategory = productCategory;
+    }
+}
---
+++ project/src/main/java/com/google/sps/data/Productset.java
@@ -0,0 +1,12 @@
+package com.google.sps.data;
+
+
+public class Productset{
+    private String setId;
+    private String setName;
+
+    public Productset(String setId, String setName){
+        this.setId = setId;
+        this.setName = setName;
+    }
+}
