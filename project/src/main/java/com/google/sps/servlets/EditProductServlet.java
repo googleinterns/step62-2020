c4970b6a0d1630a0d4832c4960680a6242d5f44d
Neel Gandhi <neelgandhi@google.com>
2020-07-09 20:24:32 +0000

    Conver editProduct file to a placeholder for now

--- project/src/main/java/com/google/sps/servlets/EditProductServlet.java
+++ project/src/main/java/com/google/sps/servlets/EditProductServlet.java
@@ -29,7 +29,7 @@ import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
 
 
-// TODO: modify this to actually do stuff to edit the product.
+
 @WebServlet("/editProduct")
 public class EditProductServlet extends HttpServlet {
 
@@ -46,75 +46,7 @@ public class EditProductServlet extends HttpServlet {
 
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    // Retrieve required parameters for the product set.
-    String productId = ServletLibrary.generateUUID();
-    String productDisplayName = request.getParameter("productDisplayName");
-    String productSetDisplayName = request.getParameter("productSetDisplayName");
-
-    // Only add the product set if doesn't already exist in the database. 
-    // Otherwise simply get the productsetId.
-    // Third parameter indicates that we are searching using the display name instead of the product set id. 
-    ProductSetEntity result = ServletLibrary.retrieveProductSetInfo(datastore, productSetDisplayName, true);
-    String productSetId = ServletLibrary.generateUUID();
-    if (result == null) {
-      Entity productSet = new Entity("ProductSet", productSetId);
-      productSet.setProperty("productSetId", productSetId);
-      productSet.setProperty("productSetDisplayName", productSetDisplayName);
-      productSet.setProperty("productIds", new ArrayList<String>());
-      datastore.put(productSet);
-    } else {
-      productSetId = result.getProductSetId();
-    }
-
-    String productCategory = request.getParameter("productCategory");
-    String businessId = userService.getCurrentUser().getUserId();
-
-    // TODO: catch any exceptions for string to float conversion.
-    float price = Float.parseFloat(request.getParameter("price"));
-
-    // TODO: support for adding multiple images. For now, we are only adding
-    // the initial image that was uploaded.
-    List<String> imageUrls = new ArrayList<>();
-    imageUrls.add(request.getParameter("mainImageUrl"));
-
-    // Get annotation and labels.
-    String cloudVisionAnnotation = request.getParameter("cloudVisionAnnotation");
-    String productDescription = request.getParameter("productDescription");
-    List<String> labels = new ArrayList<>(Arrays.asList(request.getParameterValues("labels")));
-    labels.add(productDisplayName);
-    labels.add(productSetDisplayName);
-    labels.add(productCategory); // TODO: remove the '-v1' or '-v2' at the end of the category string
-    // Remove potential duplicates, ignoring case, but preserving it when 
-    // returning the new list. We set labels to the new list that was generated.
-    Set<String> seen = new HashSet<>();
-    List<String> newLabels = new ArrayList<>();
-    for (String label : labels) {
-      if (seen.contains(label.toLowerCase())) continue;
-      seen.add(label.toLowerCase());
-      newLabels.add(label);
-    }
-    labels = newLabels;
-
-    // Create a product set entity and store in datastore.
-    Entity product = new Entity("Product", productId);
-    product.setProperty("productId", productId);
-    product.setProperty("productDisplayName", productDisplayName);
-    product.setProperty("productSetId", productSetId);
-    product.setProperty("productCategory", productCategory);
-    product.setProperty("businessId", businessId);
-    product.setProperty("price", price);
-    product.setProperty("imageUrls", imageUrls);
-    product.setProperty("labels", labels);
-    product.setProperty("productDescription", productDescription);
-    product.setProperty("cloudVisionAnnotation", new Text(cloudVisionAnnotation));
-    datastore.put(product);
-
-    // Add product to relevant tables in datastore.
-    ServletLibrary.addProductToLabels(datastore, productId, labels);
-    ServletLibrary.addProductToProductSet(datastore, productId, productSetId);
-    ServletLibrary.addProductToProductCategory(datastore, productId, productCategory);
-    ServletLibrary.addProductToBusiness(datastore, productId, businessId);
-
-    response.sendRedirect("/businessAccount.html");
+    // TODO: modify this to actually do stuff to edit the product. This is a 
+    // placeholder for now.
   }
 }
